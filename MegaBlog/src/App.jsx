27f21f1dcf19c7cc939c2ed1804dc697e3d4d301
Tajinder <txtxtx001txtx@gmail.com>
2024-12-02 13:46:09 +0530

    Initial commit

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  
+  return (
+    <div>
+      <h1>Mega Blog</h1>
+    </div>
+  )
+}
+
+export default App

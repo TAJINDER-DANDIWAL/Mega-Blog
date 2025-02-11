e5c4ec303880da935eb87372464eb27ac80e6d16
Tajinder <txtxtx001txtx@gmail.com>
2025-02-11 18:22:18 +0530

    production ready

---
+++ MegaBlog/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import {React, useState} from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../store/authSlice";
+import {Input, Button, Logo, Select} from "./index";
+import authservice from "../AppWrite/auth";
+
+export default function Login() {
+
+    return(
+        <>
+        </>
+    )
+}
---
+++ MegaBlog/src/components/index.js
@@ -0,0 +1,23 @@
+import Button from './Button';
+import Container from './Container';
+import Footer from './Footer/Footer';
+import Form from './Form';
+import Header from './Header/Header';
+import Logoutbtn from './Header/Logoutbtn';
+import Input from './Input';
+import Logo from './Logo';
+import Postcard from './Postcard';
+import Select from './Select';
+
+export {
+  Button,
+  Container,
+  Footer,
+  Form,
+  Header,
+  Logoutbtn,
+  Input,
+  Logo,
+  Postcard,
+  Select,
+};

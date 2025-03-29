934695de317ae8771f4ee8dc60f1bf4891c75655
Tajinder <txtxtx001txtx@gmail.com>
2025-03-30 04:08:18 +0530

    doing

---
+++ MegaBlog/src/components/RTE.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Editor } from '@tinymce/tinymce-react';
+import {controler} from 'react-hook-form'
+
+export default function RTE() {
+
+    return (
+        <div></div>
+    )
+}
